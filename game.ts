c5616ff5d0a5b517b8571d515c21410896cd5eb7
bassguitarbill <bassguitarbill@gmail.com>
2019-10-05 11:18:11 -0400

    Add popovers

--- game.ts
+++ game.ts
@@ -1,12 +1,22 @@
 import {Grid} from './grid.js';
 import {Hud} from './hud.js';
 import {Pawn} from './pawn.js'
+<<<<<<< HEAD
 import {Point} from './math.js';
 
 export class Game {
   readonly grid = new Grid(this, 10, 10);
   readonly pawns = new Array<Pawn>();
   readonly hud = new Hud(this);
+=======
+import {Popover, RegretPopover} from './popover.js';
+
+export class Game {
+  readonly grid = new Grid(this, 10, 10);
+  readonly mousePosition: Point = {x: 0, y: 0};
+  readonly popover : Popover;
+  readonly pawn = new Pawn(this, Math.random() * 10, Math.random() * 10, 'MOON_AGENT');
+>>>>>>> Add popovers
 
   readonly mousePosition: Point = {x: 0, y: 0};
   isMouseClicked = false;
@@ -21,10 +31,15 @@ export class Game {
     canvas.addEventListener('mouseup', () => this.isMouseClicked = false);
     canvas.addEventListener('mousedown', () => this.isMouseClicked = true);
     canvas.addEventListener('mouseout', () => this.isMouseClicked = false);
+<<<<<<< HEAD
 
     for (let i = 0; i < 100; i++){
       this.pawns.push(new Pawn(this, Math.random() * 10, Math.random() * 10, 'MOON_AGENT'));
     }
+=======
+    this.popover = RegretPopover(this.ctx);
+    this.popover.show();
+>>>>>>> Add popovers
   }
 
   tick() {
@@ -39,12 +54,17 @@ export class Game {
     this.ctx.fillRect(0, 0, w, h);
 
     this.grid.draw(this.ctx);
+<<<<<<< HEAD
     for (let i = 0; i < 100; i++){
       this.pawns[i].draw(this.ctx);
     }
     
 
     this.hud.draw(this.ctx);
+=======
+    this.pawn.draw(this.ctx);
+    this.popover.draw();
+>>>>>>> Add popovers
   }
 
   mouseMove(ev: MouseEvent) {
---
+++ popover.ts
@@ -0,0 +1,82 @@
+import { loadImage } from "./loader.js";
+
+export class PopoverButton {
+  readonly cb: (ev: MouseEvent) => void;
+  constructor(
+    readonly popover: Popover,
+    readonly x: number,
+    readonly y: number,
+    readonly image: HTMLImageElement,
+    readonly ctx : CanvasRenderingContext2D,
+    readonly callback: (ev: MouseEvent) => void
+  ) {
+    this.cb = (ev: MouseEvent) => {this.callback(ev); popover.hide()}
+    ctx.canvas.addEventListener('mouseup', this.cb);
+  }
+
+  draw() {
+    this.ctx.drawImage(this.image, this.x, this.y);
+  }
+
+  enable() {
+    this.ctx.canvas.addEventListener('mouseup', this.cb);
+  }
+
+  disable() {
+    this.ctx.canvas.removeEventListener('mouseup',  this.cb);
+  }
+}
+
+const defaultButton = loadImage('assets/pictures/button.png');
+const sorryButton = loadImage('assets/pictures/sorrybutton.png');
+
+type PopoverType = 'REGRET';
+
+export class Popover {
+  buttons : Array<PopoverButton>;
+  constructor(readonly imageName: PopoverType, readonly ctx : CanvasRenderingContext2D ) {
+    this.visible = false;
+    this.buttons = [];
+  }
+
+  visible: boolean = false;
+
+  show() {
+    this.visible = true;
+    this.buttons.forEach(b => {
+      b.enable();
+    })
+  }
+
+  hide() {
+    this.visible = false;
+    this.buttons.forEach(b => {
+      b.disable();
+    })
+  }
+
+  draw(){
+    if(this.visible) {
+      this.ctx.drawImage(images[this.imageName], 0, 0);
+      this.buttons.forEach(b => {
+        b.draw();
+      });
+    }
+  }
+}
+
+const images: {[key in PopoverType]: HTMLImageElement} = {
+  'REGRET': loadImage('assets/pictures/popovers/regret.png'),
+}
+
+export const RegretPopover = (ctx : CanvasRenderingContext2D) => {
+  const p = new Popover('REGRET', ctx);
+  p.buttons.push(new PopoverButton(
+    p, 130, 219, defaultButton, ctx, () => {}
+  ));
+  p.buttons.push(new PopoverButton(
+    p, 360, 219, sorryButton, ctx, () => {}
+  ));
+  return p;
+}
+
